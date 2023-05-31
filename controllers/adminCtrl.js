668c9baa39c3117af1fff2297702d03c88535b0c
chengwei0815 <chengwei0815@gmail.com>
2023-05-31 12:36:59 -0400

    edit isAuthenticated of admin_books in AdminCtrl

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -22,22 +22,25 @@ module.exports = {
   },
 
   admin_books: (request, response) => {
-    Book.find({}, (error, allBooks) => {
-      if (error) {
-        return error;
-      } else {
-        if (request.isAuthenticated()) {
+    if (request.isAuthenticated()) {
+      Book.find({}, (error, allBooks) => {
+        if (error) {
+          return error;
+        } else {
+
           response.render("pages/adminBooks", {
             copyrightYear: siteData.year,
             inventoryArray: allBooks,
           });
-        } else {
+        }
+      });
+    } else {
           console.log("There is an error.");
           response.redirect("/login");
         }
-      }
-    });
   },
+
+
   create_book: (request, response) => {
     response.render("pages/bookCreate", {
       copyrightYear: siteData.year,
